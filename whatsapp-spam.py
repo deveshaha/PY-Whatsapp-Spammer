50f1cf7c854bd6f5161e756f99789528584262b3
deveshaha <devesh_hanumante@outlook.com>
2022-05-17 11:32:29 +0200

    input from user

--- whatsapp-spam.py
+++ whatsapp-spam.py
@@ -2,10 +2,32 @@ import webbrowser as web
 import pyautogui
 from time import sleep
 
-web.open("https://web.whatsapp.com/send?phone=+34673295331")
+print("""\
+
+
+ __          ___    _       _______ _____         _____  _____    ______ _      ____   ____  _____  ______ _____  
+ \ \        / / |  | |   /\|__   __/ ____|  /\   |  __ \|  __ \  |  ____| |    / __ \ / __ \|  __ \|  ____|  __ \ 
+  \ \  /\  / /| |__| |  /  \  | | | (___   /  \  | |__) | |__) | | |__  | |   | |  | | |  | | |  | | |__  | |__) |
+   \ \/  \/ / |  __  | / /\ \ | |  \___ \ / /\ \ |  ___/|  ___/  |  __| | |   | |  | | |  | | |  | |  __| |  _  / 
+    \  /\  /  | |  | |/ ____ \| |  ____) / ____ \| |    | |      | |    | |___| |__| | |__| | |__| | |____| | \ \ 
+     \/  \/   |_|  |_/_/    \_\_| |_____/_/    \_\_|    |_|      |_|    |______\____/ \____/|_____/|______|_|  \_\
+                                                                                                                  
+                                                                                                                  
+
+                    """)
+
+print('Phone number: ')
+tlf = input()
+
+print('Message: ')
+msg = input()
+
+print('Number of messages: ')
+n = int(input())
+
+web.open("https://web.whatsapp.com/send?phone=" + tlf)
 sleep(10)
 
-with open("message", "r") as file:
-    for i in range(500):
-        pyautogui.typewrite("4,9 suspenso")
-        pyautogui.press("enter")
+for i in range(n):
+    pyautogui.typewrite(msg)
+    pyautogui.press('enter')
